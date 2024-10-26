f311246271cf31fc4fb8df4e4e2401f9ea43d194
Christopher DeMarco <cdemarco@drwholdings.com>
2024-10-25 21:04:14 -0400

    Update

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
+    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+end
+# <<< conda initialize <<<
+
+source /opt/homebrew/opt/asdf/libexec/asdf.fish
+
+fish_add_path $HOME/go/bin
+fish_add_path $HOME/.config/emacs/bin
+
+set -Ux EDITOR "emacsclient -nw"
+set -Ux VISUAL "emacsclient -nw"
 starship init fish | source
