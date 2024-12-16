1eaac79cb0a2d984ad3cbd1c07a78cab0d9a637a
Christopher DeMarco <cdemarco@drwholdings.com>
2024-12-16 13:21:26 -0500

    Support type/desc commit formats

--- fish/functions/gwa.fish
+++ fish/functions/gwa.fish
@@ -1,4 +1,8 @@
 function gwa --wraps='git worktree add' --description 'alias gwa=git worktree add'
-  git worktree add $argv
-        
+  git worktree add $argv $argv
+  if string match -q '*/*' $argv
+    git worktree add $argv $argv
+  else
+    git worktree add $argv
+  end
 end
