a82ead11ddedd50b8c939ff1932de799e4f95084
Christopher DeMarco <cdemarco@drwholdings.com>
2025-04-22 14:43:02 -0400

    autosave

--- doom/init.el
+++ doom/init.el
@@ -21,7 +21,7 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       company           ; the ultimate code completion backend
+       ;;company           ; the ultimate code completion backend
        ;;(corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
@@ -95,9 +95,9 @@
        editorconfig      ; let someone else argue about tabs vs spaces
        ;;ein               ; tame Jupyter notebooks with emacs
        (eval +overlay)     ; run code, run (also, repls)
-       lookup              ; navigate your code and its documentation
+       ;; lookup              ; navigate your code and its documentation
        ;;lsp (+peek)               ; M-x vscode
-       magit             ; a git porcelain for Emacs
+       ;; magit             ; a git porcelain for Emacs
        ;;make              ; run make tasks from Emacs
        ;;pass              ; password manager for nerds
        ;;pdf               ; pdf enhancements
--- fish/functions/updocsbranch.fish
+++ fish/functions/updocsbranch.fish
@@ -2,5 +2,5 @@ function updocsbranch
 cd ~/up-docs/main
 git pull
 git worktree add ../$argv[1]
-cd ../$argv[1]/pages/platform-infrastructure
+cd ../$argv[1]/pages
 end
