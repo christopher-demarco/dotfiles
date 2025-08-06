babb8e9f56302322bc7fb0690bf1bd59d1ee569d
Christopher DeMarco <cdemarco@drwholdings.com>
2025-08-06 16:41:42 -0400

    Autosave

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,12 @@ end
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
     eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
+else
+    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
+    end
 end
 # <<< conda initialize <<<
 
@@ -21,10 +27,17 @@ fish_add_path $HOME/.config/emacs/bin
 set -Ux EDITOR "emacsclient -nw"
 set -Ux VISUAL "emacsclient -nw"
 
-starship init fish | source
+command -v starship >/dev/null 2>&1 && starship init fish | source
 
-eval "$(devbox global shellenv)"
+direnv hook fish | source
+command -v starship >/dev/null 2>&1 && eval "$(devbox global shellenv)"
 
 function fish_user_key_bindings
   bind ctrl-alt-h backward-kill-word
 end
+
+# Added by Windsurf
+fish_add_path /Users/cdemarco/.codeium/windsurf/bin
+
+# Added by `rbenv init` on Wed Aug  6 15:42:25 EDT 2025
+status --is-interactive; and rbenv init - --no-rehash fish | source
--- fish/functions/updocsbranch.fish
+++ fish/functions/updocsbranch.fish
@@ -2,5 +2,5 @@ function updocsbranch
 cd ~/up-docs/main
 git pull
 git worktree add ../$argv[1]
-cd ../$argv[1]/pages
+cd ../$argv[1]/content
 end
