f2bf6889a5d244651ce65f48d303989ca80c1f6d
Christopher DeMarco <cdemarco@drwholdings.com>
2025-08-25 12:05:16 -0400

    Enable context

--- fish/functions/kpodson.fish
+++ fish/functions/kpodson.fish
@@ -1,4 +1,18 @@
 function kpodson
-kubectl get pods -A --field-selector spec.nodeName=$argv[1]
+    # Usage: kpodson [context] <node>
+    set -l ctxopt
+    set -l node
 
+    switch (count $argv)
+        case 2
+            set ctxopt --context=$argv[1]
+            set node $argv[2]
+        case 1
+            set node $argv[1]
+        case '*'
+            echo "Usage: kpodson [context] <node>" >&2
+            return 1
+    end
+
+    kubectl $ctxopt get pods -A --field-selector spec.nodeName=$node
 end
