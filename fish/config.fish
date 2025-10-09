14f7e6bc6d809ea94ab60957d4490a8e8fa34af2
Christopher DeMarco <cdemarco@drwholdings.com>
2025-10-09 16:29:52 -0400

    Autosave

--- fish/config.fish
+++ fish/config.fish
@@ -4,26 +4,13 @@ end
 
 [ (uname) = "Darwin" ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 
-# >>> conda initialize >>>
-# !! Contents within this block are managed by 'conda init' !!
-if test -f /opt/homebrew/Caskroom/miniconda/base/bin/conda
-    eval /opt/homebrew/Caskroom/miniconda/base/bin/conda "shell.fish" "hook" $argv | source
-else
-    if test -f "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-        . "/opt/homebrew/Caskroom/miniconda/base/etc/fish/conf.d/conda.fish"
-    else
-        set -x PATH "/opt/homebrew/Caskroom/miniconda/base/bin" $PATH
-    end
-end
-# <<< conda initialize <<<
-
 [ -e /opt/homebrew/opt/asdf/libexec/asdf.fish ] && . /opt/homebrew/opt/asdf/libexec/asdf.fish
 [ -e ~/.work.fish ] && . ~/.work.fish
-
 
 fish_add_path $HOME/go/bin
 fish_add_path $HOME/.config/emacs/bin
 fish_add_path $HOME/bin
+fish_add_path $HOME/cmd/src/remindme
 
 set -Ux EDITOR "emacsclient -nw"
 set -Ux VISUAL "emacsclient -nw"
@@ -37,8 +24,6 @@ function fish_user_key_bindings
   bind ctrl-alt-h backward-kill-word
 end
 
-# Added by Windsurf
-fish_add_path /Users/cdemarco/.codeium/windsurf/bin
 
 # Added by `rbenv init` on Wed Aug  6 15:42:25 EDT 2025
 status --is-interactive; and rbenv init - --no-rehash fish | source
